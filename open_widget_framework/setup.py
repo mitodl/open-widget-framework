c6b9509e1cca33bcdbac7f598aa10321c23e8857
Jake Klingensmith <jake@zagaran.com>
2018-10-25 11:30:59 -0400

    update setup version

--- open_widget_framework/setup.py
+++ open_widget_framework/setup.py
@@ -30,7 +30,7 @@ if sys.version < '3':
 
 setup(
     name="open_widget_framework",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     license='MIT',
     author="Zagaran, Inc.",
