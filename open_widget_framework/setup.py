a69429c373643595c9b691f200b12cafb28571f9
Jake Klingensmith <jake@zagaran.com>
2018-10-25 11:51:03 -0400

    update setup.py version number

--- open_widget_framework/setup.py
+++ open_widget_framework/setup.py
@@ -30,7 +30,7 @@ if sys.version < '3':
 
 setup(
     name="open_widget_framework",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=[
             "Django>=2.1.2",
