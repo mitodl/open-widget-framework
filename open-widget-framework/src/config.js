ce881cc0b23c36f6856a3b45cd45d728f0d828c0
Jake Klingensmith <jake@zagaran.com>
2018-11-14 14:56:47 -0500

    Integrated PR #12 into new code
    Minor aesthetic changes

--- open-widget-framework/src/config.js
+++ open-widget-framework/src/config.js
@@ -1,25 +1,23 @@
 import {_defaultRenderer, _defaultWidgetWrapper, _defaultListWrapper,
   _defaultFormWrapper, _defaultLoader, _defaultFetchJsonData} from './defaults'
 
-function configureWidgetFrameworkSettings(userSettings) {
-  if (userSettings === undefined) {
-    userSettings = {}
-  }
-
-  let SETTINGS = {
-    csrfToken: userSettings.csrfToken || '',
-    defaultFormWrapper: userSettings.defaultFormWrapper || _defaultFormWrapper,
-    defaultListWrapper: userSettings.defaultListWrapper || _defaultListWrapper,
-    defaultRenderer: userSettings.defaultRenderer || _defaultRenderer,
-    defaultWidgetWrapper: userSettings.defaultWidgetWrapper || _defaultWidgetWrapper,
-    disableWidgetFramework: userSettings.disableWidgetFramework || false,
-    errorHandler: userSettings.errorHandler || console.error,
-    fetchData: userSettings.fetchData || _defaultFetchJsonData,
-    loader: userSettings.loader || _defaultLoader,
-    renderers: userSettings.renderers || {},
-  }
-
-  return SETTINGS
+const defaultSettings = {
+  FormWrapper: _defaultFormWrapper,
+  ListWrapper: _defaultListWrapper,
+  defaultRenderer: _defaultRenderer,
+  WidgetWrapper: _defaultWidgetWrapper,
+  disableWidgetFramework: false,
+  errorHandler: console.error,
+  fetchData: _defaultFetchJsonData,
+  loader: _defaultLoader,
+  renderers: {},
 }
 
-export default configureWidgetFrameworkSettings
+function configureWidgetFrameworkSettings(userSettings = {}) {
+  return {
+    ...defaultSettings,
+    ...userSettings
+  }
+}
+
+export { defaultSettings, configureWidgetFrameworkSettings }
